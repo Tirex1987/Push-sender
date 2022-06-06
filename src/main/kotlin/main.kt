aa198676bf57c8d4e8a7da6de570d598c33a22bc
Tirex1987 <tirex1987@yandex.ru>
2022-06-07 01:24:50 +0300

    Add unknown type message and message for new post

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,24 +6,44 @@ import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import java.io.FileInputStream
+import java.lang.Thread.sleep
 
 fun main() {
+
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .build()
 
     FirebaseApp.initializeApp(options)
 
-    val message = Message.builder()
-        .putData("action", "LIKE")
-        .putData("content", """{
+    sendMessage("LIKE", """{
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
           "postAuthor": "Netology"
         }""".trimIndent())
-        .setToken(token)
+
+    sleep(1000)
+
+    sendMessage("UNKNOWN", "")
+
+    sleep(1000)
+
+    sendMessage("NEW_POST", """{
+          "userId": 1,
+          "userName": "Alexey",
+          "postId": 2,
+          "title": "Pull requests",
+          "content": "When merging pull requests, you can allow any combination of merge commits, squashing, or rebasing. At least one option must be enabled. If you have linear history requirement enabled on any protected branch, you must enable squashing or rebasing."
+        }""".trimIndent())
+}
+
+private fun sendMessage(actionType: String, actionContent: String) {
+    val messageNewPost = Message.builder()
+        .putData("action", actionType)
+        .putData("content", actionContent)
+        .setToken("fJJWoWeQQc2IBhibichTGx:APA91bF4nNmdJIFmxc7Y4nL2a-7fLxSbTicPa7KV38AaMxlGHKi5a01ncq9bsgV-5cNPs17v0XK_oVeLCGny8rrkYdm3AFxB0k6z-hg5pqUNETArRYTOT9ysbzzPboV7IxZ2lWdH-J_H")
         .build()
 
-    FirebaseMessaging.getInstance().send(message)
+    FirebaseMessaging.getInstance().send(messageNewPost)
 }
